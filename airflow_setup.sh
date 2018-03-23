db6fd1caad727a3c0890bd3038897f3b781474eb
Ubuntu <ubuntu@ip-172-31-9-243.us-east-2.compute.internal>
2018-03-23 15:18:23 +0000

    WIP: have rest of airflow_setup.sh fleshed out, retest against EC2 role, verify that with User Data, it can fetch from S3 and init airflow db Then we just have to uncomment the rest of airflow_setup.sh and verify that we can connect to the webserver from my ip:

--- airflow_setup.sh
+++ airflow_setup.sh
@@ -7,16 +7,14 @@ mv -n ../skills/lib/python3.5/site-packages/airflow/example_dags\
 
 airflow initdb
 
-# Fix up airflow.cfg
-cat config/airflow_extras.cfg >> $AIRFLOW_HOME/airflow.cfg
-
-#sed -i '/dags_folder = */c\dags_folder = '"$local_repo"'skills-airflow/dags' $AIRFLOW_HOME/airflow.cfg
+## Fix up airflow.cfg
+#cat config/airflow_extras.cfg >> $AIRFLOW_HOME/airflow.cfg
 #
-## Airflow needs a path from which to look for skills-airflow library
+## Stand up Airflow, EC2 inbound should be set for your IP to look at the webserver
 #
-## Initalize Airflow database on first run, kick off a scheduler (sequential) and webserver
-## Also shutdown any current webserver already running
-##cat $AIRFLOW_HOME/airflow-webserver.pid | xargs kill -9
-##airflow scheduler &
-##airflow webserver -hn 127.0.0.1 -p 8080 &
-###google-chrome-stable --args 'http://127.0.0.1:8080'
+## ... shutdown any current webserver already running
+#cat $AIRFLOW_HOME/airflow-webserver.pid | xargs kill -9
+#
+## ... stand up airflow, note host on 0.0.0.0, assume EC2 inbound protects
+#airflow scheduler &
+#airflow webserver -hn 0.0.0.0 -p 8080 &
