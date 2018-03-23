085f9eede4c174b37766856ea437cdd39e06b22c
Ubuntu <ubuntu@ip-172-31-9-129.us-east-2.compute.internal>
2018-03-23 19:38:40 +0000

    WIP: tweaks files for append to airflow.cfg

--- airflow_setup.sh
+++ airflow_setup.sh
@@ -8,13 +8,13 @@ mv -n ../skills/lib/python3.5/site-packages/airflow/example_dags\
 airflow initdb
 
 ## Fix up airflow.cfg
-#cat config/airflow_extras.cfg >> $AIRFLOW_HOME/airflow.cfg
-#
-## Stand up Airflow, EC2 inbound should be set for your IP to look at the webserver
-#
-## ... shutdown any current webserver already running
-#cat $AIRFLOW_HOME/airflow-webserver.pid | xargs kill -9
-#
-## ... stand up airflow, note host on 0.0.0.0, assume EC2 inbound protects
-#airflow scheduler &
-#airflow webserver -hn 0.0.0.0 -p 8080 &
+cat config/airflow_extras.cfg >> $AIRFLOW_HOME/airflow.cfg
+
+# Stand up Airflow, EC2 inbound should be set for your IP to look at the webserver
+
+# ... shutdown any current webserver already running
+cat $AIRFLOW_HOME/airflow-webserver.pid | xargs kill -9
+
+# ... stand up airflow, note host on 0.0.0.0, assume EC2 inbound protects instance
+airflow scheduler &
+airflow webserver -hn 0.0.0.0 -p 8080 &
