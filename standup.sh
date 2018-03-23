e6314d0cf7d283c9ef3b38148266b8423bbdd0db
Ubuntu <ubuntu@ip-172-31-9-243.us-east-2.compute.internal>
2018-03-23 13:58:55 +0000

    WIP: modifying standup for EC2 use, offloaded work to User Data script

--- standup.sh
+++ standup.sh
@@ -1,37 +1,20 @@
-#!./env/bin/python
+# This stands up alembic, skills-airflow, reads in any dags in the dag/
+# Assumes that EC2 User Data sets up access to RDS, S3 and that
+# IAM Role ACECredly-S3-Access is assigned (for fetching RDS credentials
+# and reading/writing to S3 bucket)
 
-# This is a quick and dirty script that stands up skills-airflow
-# against the robinsonkwame/skills-airflow fork (modifications made
-# so alembic wouldn't crash on env.py, etc. etc.
-
-# It assumes resources are stood up on EC2, RDS, S3 and stands up
-# skills-airflow aginst them.
-
-#echo "... Standing up local elasticsearch, postgres"
-#docker-compose down
-#cd db
-#docker rm postgres_1
-#docker build .
-#cd ..
-#docker-compose down
-#docker-compose up&
-## gives the services some time to stand up
-#sleep 15
-
+source ../skills/bin/activate
 
 echo "... upgrading databases (alembic)"
 alembic upgrade head
 
-#echo "... launching airflow, note .pid file, a browser will open up"
-#export AIRFLOW_HOME=${AIRFLOW_HOME-~/airflow/}
-#./airflow_setup.sh&
+echo "... launching airflow, note .pid file, a browser will open up"
+export AIRFLOW_HOME=${AIRFLOW_HOME-~/}
+./airflow_setup.sh&
+
 #
 #echo " ... appending extra locations to the config file"
-#cat config/airflow_extras.cfg >> airflow/airflow.cfg
-#
-#echo " ... setting ES endpoint variable"
-## not tested, only used for ES normalizer
-#export ELASTICSEARCH_ENDPOINT=http://127.0.0.1/bank/_search?
+#cat config/airflow_extras.cfg >> airflow.cfg
 #
 #echo " ... running the scheduler!"
 ##airflow scheduler&
